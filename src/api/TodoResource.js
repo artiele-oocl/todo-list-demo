c54e60af708da13851d234e3633373d542bb4706
artiele-oocl <artiele@oocl.com>
2019-10-23 12:24:09 +0800

    Update port

--- src/api/TodoResource.js
+++ src/api/TodoResource.js
@@ -1,10 +1,10 @@
 export default {
   getAll: () => fetch(
-    "http://localhost:8080/api/todos",
+    "http://localhost:9090/api/todos",
     {mode: 'cors'}
     ),
   createTodo: (todo) => fetch(
-    "http://localhost:8080/api/todos", {
+    "http://localhost:9090/api/todos", {
       mode: 'cors',
       method: 'POST',
       headers: new Headers({'Content-Type': 'application/json'}),
